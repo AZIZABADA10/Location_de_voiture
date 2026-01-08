373f10b8ba55f178da739b064521478a87cb6a20
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-08 10:26:38 +0000

    feat(tag): ajout de la classe Tag avec CRUD complet

---
+++ Classes/Tag.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Classes;
+
+use App\Config\Database;
+use PDO;
+
+class Tag
+{
+    private ?int $id_tag;
+    private string $tag;
+
+    public function __construct(?int $id_tag, string $tag)
+    {
+        $this->id_tag = $id_tag;
+        $this->tag = $tag;
+    }
+
+    public function ajouterTag(): bool
+    {
+        $con = Database::getInstance()->getConnexion();
+        $sql = "INSERT INTO Tag (tag) VALUES (?)";
+        $stmt = $con->prepare($sql);
+
+        return $stmt->execute([$this->tag]);
+    }
+
+    public function modifierTag(int $id): bool
+    {
+        $con = Database::getInstance()->getConnexion();
+        $sql = "UPDATE Tag SET tag = ? WHERE id_tag = ?";
+        $stmt = $con->prepare($sql);
+
+        return $stmt->execute([
+            $this->tag,
+            $id
+        ]);
+    }
+
+    public function supprimerTag(int $id): bool
+    {
+        $con = Database::getInstance()->getConnexion();
+        $sql = "DELETE FROM Tag WHERE id_tag = ?";
+        $stmt = $con->prepare($sql);
+
+        return $stmt->execute([$id]);
+    }
+
+    public function getAllTag(): array
+    {
+        $con = Database::getInstance()->getConnexion();
+        $sql = "SELECT * FROM Tag";
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function getTagById(int $id): ?object
+    {
+        $con = Database::getInstance()->getConnexion();
+        $sql = "SELECT * FROM Tag WHERE id_tag = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->execute([$id]);
+
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+}
